1634dd540807c6a32b4d1af859c58e596c2f010e
Sekine <tenkoru55@gmail.com>
2020-09-13 22:45:07 +0900

    ABC178つらい

---
+++ ABC178/B.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+using namespace std;
+
+#define _overload(_1, _2, _3, name, ...) name
+#define _rep(i, n) for(ll i = 0; i < (ll)n; i++)
+#define _reps(i, s, n) for(ll i = (ll)(s); i < (ll)n; i++)
+#define REP(...) _overload(__VA_ARGS__, _reps, _rep,)(__VA_ARGS__)
+#define all(v) (v).begin(),(v).end()
+#define UNIQUE(v) (v).erase( unique( all(v) ), (v).end() );
+#define MOD 1000000007
+
+int main(){
+	ll n[4];	cin >> n[0] >> n[1] >> n[2] >> n[3];
+
+	cout << max({n[0]*n[2], n[0]*n[3], n[1]*n[2], n[1]*n[3]});
+
+	return 0;
+}
