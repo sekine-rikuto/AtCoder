94f87936fdbcc4da30f4c469111a00e4f8085e50
Sekine <tenkoru55@gmail.com>
2020-10-13 14:56:16 +0900

    ARC105 AB早解きで入緑

---
+++ ARC105/A.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+//#include <atcoder/all>
+
+typedef long long ll;
+using namespace std;
+//using namespace atcoder;
+
+#define _overload(_1, _2, _3, name, ...) name
+#define _rep(i, n) for(ll i = 0; i < (ll)n; i++)
+#define _reps(i, s, n) for(ll i = (ll)(s); i < (ll)n; i++)
+#define REP(...) _overload(__VA_ARGS__, _reps, _rep,)(__VA_ARGS__)
+#define all(v) (v).begin(),(v).end()
+#define UNIQUE(v) (v).erase( unique( all(v) ), (v).end() );
+#define MOD 1000000007
+
+int main(){
+	ll a, b, c, d;	cin >> a >> b >> c >> d;
+	ll eat, other;
+	bool ans = false;
+
+	REP(i, 16){
+		eat = 0;
+		other = 0;
+
+		if( i & (1<<1) )	eat += a;
+		else				other += a;
+
+		if( i & (1<<2) )	eat += b;
+		else				other += b;
+
+		if( i & (1<<3) )	eat += c;
+		else				other += c;
+
+		if( i & (1<<4) )	eat += d;
+		else				other += d;
+
+		if( eat == other )	ans = true;
+	}
+
+	if( ans )	cout << "Yes" << endl;
+	else		cout << "No" << endl;
+
+	return 0;
+}
